05c8f0cdee9c3bf52b48762ef5a328930636508d
Ariel Barcellos <barcellos.ariel@gmail.com>
2019-04-05 10:57:58 -0500

    completed

--- index.js
+++ index.js
@@ -3,11 +3,29 @@
   and returns the first recurring character
   'ABCA' => 'A'
   'ABCBA' => 'B'
-  'ABC' => B
+  'ABC' => null
 */
 
 const tests = [
   'ABCA',
   'ABCBA',
-  'ABC'
+  'ABC',
+  'ahsudhjdoas',
+  'dfu2dqxhe1qwusdhq'
 ]
+
+const getFirstRecurringChar = given_string => {
+  const recurring = []
+
+  for (let char of given_string) {
+    let index =  recurring.indexOf(char)
+    if (index !== -1) {
+      return char
+    }
+    recurring.push(char)
+  }
+
+  return null
+}
+
+tests.forEach(test => console.log(getFirstRecurringChar(test)))
